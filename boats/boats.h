c939f674892c61fac55eb5cd7dd43436d82a0ad1
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-01 01:07:23 +0300

    Boats, tests

---
+++ boats/boats.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+
+using Matrix = std::vector<std::vector<char>>;
+
+
+inline std::size_t calcBoats(const Matrix&) {
+    return 0;
+}
---
+++ boats/test_boats.cpp
@@ -0,0 +1,96 @@
+#include "boats.h"
+#include <gtest/gtest.h>
+
+
+// Есть матрица NxN, состоящая из 0 и 1, и отражающая расположения кораблей на поле для морского боя.
+
+// Поле может быть любого размера, но обязательно квадратное.
+// Кораблей может быть любое количество
+// Размер кораблей — от 1х1 до 1хN
+// Корабли никак не соприкасаются друг с другом.
+// Могут располагаться горизонтально и вертикально
+// Необходимо подсчитать количество кораблей.
+
+TEST(boats, empty) {
+    Matrix matrix = {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 0);
+}
+
+TEST(boats, a) {
+    Matrix matrix = {
+        {4, 0, 0, 0},
+        {4, 0, 2, 2},
+        {4, 0, 0, 0},
+        {4, 0, 0, 1}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 3);
+}
+
+TEST(boats, b) {
+    Matrix matrix = {
+        {0, 4, 0, 0},
+        {0, 4, 0, 1},
+        {0, 4, 0, 0},
+        {0, 4, 0, 1}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 3);
+}
+
+TEST(boats, c) {
+    Matrix matrix = {
+        {3, 0, 0, 4},
+        {3, 0, 0, 4},
+        {3, 0, 0, 4},
+        {0, 0, 0, 4}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 2);
+}
+
+TEST(boats, d) {
+    Matrix matrix = {
+        {2, 0, 0, 4},
+        {2, 0, 0, 4},
+        {0, 1, 0, 4},
+        {1, 0, 0, 4}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 4);
+}
+
+TEST(boats, e) {
+    Matrix matrix = {
+        {4, 4, 4, 4},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 3, 3, 3}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 2);
+}
+
+TEST(boats, f) {
+    Matrix matrix = {
+        {3, 3, 3, 0},
+        {0, 0, 0, 1},
+        {0, 0, 0, 0},
+        {4, 4, 4, 4}
+    };
+
+    std::size_t result = calcBoats(matrix);
+    ASSERT_EQ(result, 3);
+}
