6eb90a4f3ef409ed5d702ef1e54aa7477d47d20e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-03 15:51:01 +0300

    Uniteller, Symbol increment

--- uniteller/test_id.cpp
+++ uniteller/test_id.cpp
@@ -10,6 +10,9 @@ class Symbol
 public:
     Symbol(char);
 
+    char get() const;
+    bool increment();
+
 private:
     char value;
     const std::vector<char> excluded = {'D', 'F', 'G', 'J', 'M', 'Q', 'V'};
@@ -28,6 +31,23 @@ Symbol::Symbol(char c) {
     value = c;
 }
 
+char Symbol::get() const {
+    return value;
+}
+
+bool Symbol::increment() {
+    for (;;) {
+        value++;
+
+        auto it = std::find(std::begin(excluded), std::end(excluded), value);
+        if (it == std::end(excluded)) {
+            break;
+        }
+    }
+
+    return false;
+}
+
 
 TEST(Symbol, ctor) {
     ASSERT_NO_THROW( Symbol{'A'}; );
@@ -42,4 +62,46 @@ TEST(Symbol, ctor) {
     ASSERT_ANY_THROW( Symbol{'M'}; );
     ASSERT_ANY_THROW( Symbol{'Q'}; );
     ASSERT_ANY_THROW( Symbol{'V'}; );
+}
+
+TEST(Symbol, increment) {
+    Symbol s{'B'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'C');
+}
+
+TEST(Symbol, skip_D) {
+    Symbol s{'C'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'E');
+}
+
+TEST(Symbol, skip_F_G) {
+    Symbol s{'E'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'H');
+}
+
+TEST(Symbol, excluded_J) {
+    Symbol s{'I'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'K');
+}
+
+TEST(Symbol, excluded_M) {
+    Symbol s{'L'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'N');
+}
+
+TEST(Symbol, excluded_Q) {
+    Symbol s{'P'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'R');
+}
+
+TEST(Symbol, excluded_V) {
+    Symbol s{'U'};
+    s.increment();
+    ASSERT_EQ(s.get(), 'W');
 }
