75d7c8a35795a13ee7708776ff8a69530bf83bb3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-04 15:46:53 +0300

    Uniteller, Item increment

--- uniteller/item.cpp
+++ uniteller/item.cpp
@@ -1,5 +1,6 @@
 #include "item.h"
 
+
 Item::Item(const Symbol& s, const Digit& d)
     :
       symbol{s},
@@ -8,4 +9,14 @@ Item::Item(const Symbol& s, const Digit& d)
 
 std::string Item::get() const {
     return std::string{symbol.get(), digit.get()};
+}
+
+bool Item::increment() {
+    bool digitOverflow = digit.increment();
+    if (digitOverflow) {
+        bool symbolOverflow = symbol.increment();
+        return symbolOverflow;
+    }
+
+    return false;
 }
--- uniteller/test_item.cpp
+++ uniteller/test_item.cpp
@@ -7,3 +7,21 @@ TEST(Item, get) {
     Item item{ Symbol{'B'}, Digit{'7'} };
     ASSERT_EQ(item.get(), "B7");
 }
+
+TEST(Item, increment) {
+    Item item{ Symbol{'C'}, Digit{'8'} };
+    ASSERT_FALSE(item.increment());
+    ASSERT_EQ(item.get(), "C9");
+}
+
+TEST(Item, overflow_digit) {
+    Item item{ Symbol{'A'}, Digit{'9'} };
+    ASSERT_FALSE(item.increment());
+    ASSERT_EQ(item.get(), "B1");
+}
+
+TEST(Item, overflow_all) {
+    Item item{ Symbol{'Z'}, Digit{'9'} };
+    ASSERT_TRUE(item.increment());
+    ASSERT_EQ(item.get(), "A1");
+}
