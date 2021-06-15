881febd1c70351e94f8d990ca2d9e80ffe7d49ce
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-16 00:11:47 +0300

    Nice numbers13, Digit sum

--- nice_numbers13/digit.h
+++ nice_numbers13/digit.h
@@ -13,6 +13,9 @@ public:
 
     struct Less;
     struct Equal;
+    struct Sum;
+
+    static Sum sum(const Digit&, const Digit&);
 
 private:
     static const unsigned char top = 12;
@@ -27,6 +30,33 @@ inline Digit::Digit(unsigned char v)
     if (v > top) {
         throw std::logic_error{"Digit: overflow"};
     }
+}
+
+
+struct Digit::Sum
+{
+    Digit value;
+    Digit carry;
+};
+
+Digit::Sum Digit::sum(const Digit& a, const Digit& b) {
+    auto s = a.value + b.value;
+    if (s <= top) {
+        return Sum{
+            Digit{static_cast<unsigned char>(s)},
+            Digit{0}
+        };
+    }
+    else {
+        return Sum{
+            Digit{static_cast<unsigned char>(s - top)},
+            Digit{1}
+        };
+    }
+}
+
+Digit::Sum operator+ (const Digit& a, const Digit& b) {
+    return Digit::sum(a, b);
 }
 
 
--- nice_numbers13/test_digit.cpp
+++ nice_numbers13/test_digit.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 
-TEST(Digit, ctor) {
+TEST(NiceNumbers13_Digit, ctor) {
     EXPECT_NO_THROW(Digit(0));
     EXPECT_NO_THROW(Digit(7));
     EXPECT_NO_THROW(Digit(12));
@@ -10,7 +10,38 @@ TEST(Digit, ctor) {
     EXPECT_ANY_THROW(Digit(42));
 }
 
-TEST(Digit, equal) {
+TEST(NiceNumbers13_Digit, equal) {
     EXPECT_EQ(Digit(5), Digit{5});
     EXPECT_NE(Digit(8), Digit{5});
+}
+
+
+TEST(NiceNumbers13_Digit, sum) {
+    Digit a{2};
+    Digit b{3};
+
+    Digit::Sum result = a + b;
+
+    EXPECT_EQ(result.value, Digit{5});
+    EXPECT_EQ(result.carry, Digit{0});
+}
+
+TEST(NiceNumbers13_Digit, sum_carry) {
+    Digit a{7};
+    Digit b{8};
+
+    Digit::Sum result = a + b;
+
+    EXPECT_EQ(result.value, Digit{3});
+    EXPECT_EQ(result.carry, Digit{1});
+}
+
+TEST(NiceNumbers13_Digit, sum_carry2) {
+    Digit a{12};
+    Digit b{12};
+
+    Digit::Sum result = a + b;
+
+    EXPECT_EQ(result.value, Digit{12});
+    EXPECT_EQ(result.carry, Digit{1});
 }
